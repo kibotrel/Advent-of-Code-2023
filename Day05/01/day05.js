2b682f619fb09edbc0ac51e1f5bac27741aece87
BOTREL Kilian <botrel.kilian1999@gmail.com>
2023-12-05 08:22:45 +0100

    wip: 🚧 day 05 part 01

---
+++ Day05/01/day05.js
@@ -0,0 +1,92 @@
+const { log } = require("console");
+const fs = require("fs");
+const { performance } = require("perf_hooks");
+
+const start = performance.now();
+const input = fs.readFileSync("../input.txt", "utf8").split("\n");
+
+const STAGES = {
+  SEEDS: 0,
+  SOIL: 1,
+  FERTILIZER: 2,
+  WATER: 4,
+  LIGHT: 8,
+  TEMPERATURE: 16,
+  HUMIDITY: 32,
+  LOCATION: 64,
+};
+
+const seeds = [];
+const maps = new Map([
+  [STAGES.SOIL, []],
+  [STAGES.FERTILIZER, []],
+  [STAGES.WATER, []],
+  [STAGES.LIGHT, []],
+  [STAGES.TEMPERATURE, []],
+  [STAGES.HUMIDITY, []],
+  [STAGES.LOCATION, []],
+]);
+
+let stage = STAGES.SEEDS;
+let response = 0;
+
+input.forEach((line, lineNumber) => {
+  if (line.length === 0) {
+    return;
+  }
+
+  switch (line) {
+    case "seed-to-soil map:": {
+      stage = STAGES.SOIL;
+      return;
+    }
+    case "soil-to-fertilizer map:": {
+      stage = STAGES.FERTILIZER;
+      return;
+    }
+    case "fertilizer-to-water map:": {
+      stage = STAGES.WATER;
+      return;
+    }
+    case "water-to-light map:": {
+      stage = STAGES.LIGHT;
+      return;
+    }
+    case "light-to-temperature map:": {
+      stage = STAGES.TEMPERATURE;
+      return;
+    }
+    case "temperature-to-humidity map:": {
+      stage = STAGES.HUMIDITY;
+      return;
+    }
+    case "humidity-to-location map:": {
+      stage = STAGES.LOCATION;
+      return;
+    }
+    default: {
+      if (stage === STAGES.SEEDS) {
+        const [_, seedNumbers] = line.split(": ");
+
+        seeds.push(...seedNumbers.split(" ").map((n) => parseInt(n, 10)));
+      } else {
+        const stageMap = maps.get(stage);
+        const [destinationStart, sourceStart, rangeLength] = line
+          .split(" ")
+          .map((n) => parseInt(n, 10));
+
+        stageMap.push([
+          [sourceStart, sourceStart + rangeLength - 1],
+          [destinationStart, destinationStart + rangeLength - 1],
+        ]);
+      }
+    }
+  }
+});
+
+console.log(seeds);
+console.log(maps);
+
+const end = performance.now();
+
+console.log(`Execution time: ${(end - start) / 1000} seconds`);
